aa72d6ec704423374ac5d33c6e3c41c3ed23ae87
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-09 22:07:32 +0100

    intro to styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
+//styles
+import GlobalStyle from "./components/GlobalStyle";
+
+
 function App() {
   return (
     <div className ="App">
       <AboutUs/>
-      
+      <GlobalStyle/>
     </div>
   );
 }
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,34 +1,64 @@
 import React from "react";
-import home1 from "../img/home1.png"
+import home1 from "../img/home1.png";
+//Styling
+import styled from 'styled-components';
 
 
 const AboutSection =() =>{
 
 
     return(
-    <div>
-        <div className="description">
+    <About>
+        <Description>
             <div className ="title">
-                <div className ="hide">
+                <Hide>
                     <h2> I work to make </h2>
-                </div>
-                <div className ="hide">
+                </Hide>
+                <Hide>
                     <h2> your <span>dreams </span> come </h2>
-                </div>
-                <div className ="hide">
+                </Hide>
+                <Hide>
                     <h2> true. </h2>
-                </div>
+                </Hide>
                 </div>
                 <p> Reach out to me for any designs or developer jobs and ideas. 
                 I am a professional with amazing skills</p>
                 <button> Contact Us</button>
-                </div>
-                <div className="img">
+                </Description>
+                <Image>
                     <img src={home1} alt="guy with a camera"/>
-                </div>
-            </div>
+                </Image>
+            </About>
 )
 }
+//styled components
+const About = styled.div`
+min-height:90vh;
+display: flex;
+justify-content:space-between;
+color:white;
+padding:5rem;
+align-items:center;`
 
+const Description = styled.div`
+flex:1 ;
+padding-right:5rem ;
+h2{
+    font-weight:lighter ;
+}
+ `
+
+ const Image = styled.div`
+ flex:1;
+ overflow:hidden ;
+ img{
+     width:100%;
+     height:80vh ;
+     object-fit:cover ;
+ }
+ `
+
+ const Hide = styled.div`
+ overflow:hidden;`
 
 export default AboutSection;
