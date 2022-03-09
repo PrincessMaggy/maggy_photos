4447bc2036a8954a072bc9860a4638bf1b6b7e7c
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-09 13:31:27 +0100

    Fixed bug

---
+++ src/AboutSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutSection =() =>{
+
+
+    return(
+    <div></div>
+)
+}
+
+
+export default AboutSection;
--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from "react";
 function App() {
   return (
     <div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    
+      <header>MaggyLab</header>
+      
     </div>
   );
 }
