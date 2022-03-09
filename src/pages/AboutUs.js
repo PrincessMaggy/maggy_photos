70080d83e24ad93cebfbe11f55baf4a9730627ec
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-09 14:10:25 +0100

    services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,16 @@
 import React from 'react';
 //Page Components
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+
+
 const AboutUs =() =>{
 
     return (
-        <div>
+        <>
             <AboutSection/>
-        </div>
+            <ServicesSection/>
+        </>
     )
 
 }
