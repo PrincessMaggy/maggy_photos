dfa9a908ff4b1d3df6fed77b1e925e414d1ba26c
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-09 13:01:49 +0100

    Added and removed files needed for the project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +10,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+    
     </div>
   );
 }
