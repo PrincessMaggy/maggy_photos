79e9c11e8cee96630ea42d3f3cc92f8201366ee2
PrincessMaggy <edoziemagdalene@gmail.com>
2022-03-09 14:24:09 +0100

    faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+
+const FaqSection =()=>{
+    return(
+        <div className="faq">
+            <h2> Any Questions <span> FAQ</span></h2>
+            <div className=" question">
+                <h4> How Do I Start?</h4>
+                <div className="answer">
+                    <p> Excepteur voluptate aute incididunt.</p>
+                    <p>Irure consectetur dolor eu sunt laborum sint id et cupidatat pariatur anim do fugiat.</p>
+                </div>
+            </div>
+
+            <div className=" question">
+                <h4> Daily Schedule?</h4>
+                <div className="answer">
+                    <p> Excepteur voluptate aute incididunt.</p>
+                    <p>Irure consectetur dolor eu sunt laborum sint id et cupidatat pariatur anim do fugiat.</p>
+                </div>
+            </div>
+
+            <div className=" question">
+                <h4>Different Payment Methods?</h4>
+                <div className="answer">
+                    <p> Excepteur voluptate aute incididunt.</p>
+                    <p>Irure consectetur dolor eu sunt laborum sint id et cupidatat pariatur anim do fugiat.</p>
+                </div>
+            </div>
+
+            <div className=" question">
+                <h4> What Products Do You Offer?</h4>
+                <div className="answer">
+                    <p> Excepteur voluptate aute incididunt.</p>
+                    <p>Irure consectetur dolor eu sunt laborum sint id et cupidatat pariatur anim do fugiat.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react';
 //Page Components
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import FaqSection from '../components/FaqSection';
 
 
 const AboutUs =() =>{
@@ -10,6 +11,7 @@ const AboutUs =() =>{
         <>
             <AboutSection/>
             <ServicesSection/>
+            <FaqSection/>
         </>
     )
 
