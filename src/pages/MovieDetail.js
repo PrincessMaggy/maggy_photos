702cd73b5666a762cfca9d3f72646f9b85f7bed6
PrincessMaggy <edoziemagdalene@gmail.com>
2022-08-10 17:46:59 +0100

    finally some progress

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -18,12 +18,93 @@ useEffect(()=>{
 },[movies,url])
 
 return(
-    <div>
-        <h1>Movie Detail</h1>
-    </div>
+    <>
+    {movie && (
+    <Detail>
+        <Headline>
+            <h1>{movie.title}</h1>
+            <img src={movie.mainImg}  alt=""/>
+        </Headline>
+        <Awards>
+            {movie.awards.map((award) =>(
+                <Award
+                title={award.title}
+                key= {award.title}
+                description={award.description}/>
+            ))}
+        </Awards>
+        <ImageDisplay>
+            <img src={movie.secondaryImg} alt="movie" />
+        </ImageDisplay>
+    </Detail>
+    )}</>
 )
 
 }
+const Detail =styled.div`
+    color:white;
+`;
+
+const Headline = styled.div`
+    min-height:90vh;
+    padding-top: 20vh;
+    position:relative;
+    h2{
+        position: absolute;
+        top: 10%;
+        left:50%;
+        transform: translate(-50%, -10%);
+    }
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit:cover;
+    }
+`;
+const Awards = styled.div`
+    min-height:80vh;
+    display:flex;
+    margin: 5rem 10rem;
+    align-items:center;
+    justify-content:space-around;
+`
+
+const AwardStyle = styled.div`
+    padding:5rem;
+    h3{
+        font-size:2rem;
+    }
+    .line{
+        width: 100%;
+        background: #23d997;
+        height: 0.5rem;
+        margin: 1rem 0rem;
+    }
+    p{
+        padding: 2rem 0rem;
+    }
+`;
+const ImageDisplay= styled.div`
+    min-height:50vh;
+    img{
+        width: 100%;
+        height: 100vh;
+        object-fit:cover;
+    }
+`
+        
+// Award component
+
+const Award =(props) =>{
+
+return(
+    <AwardStyle>
+        <h3>{props.title}</h3>
+        <div className="line"></div>
+        <p>{props.description}</p>
+    </AwardStyle>
+)
 
 
+}
 export default MovieDetail;
