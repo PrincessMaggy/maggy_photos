a820f1ebb97413f5a576213a55212fdbcec5189f
PrincessMaggy <edoziemagdalene@gmail.com>
2022-08-11 10:29:13 +0100

    ourwork animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -11,7 +11,7 @@ import goodtimes from '../img/goodtimes-small.png';
 
 //animations
 import {motion} from 'framer-motion'
-import { pageAnimation } from '../animation';
+import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from '../animation';
 
 const OurWork =()=>{
 
@@ -22,11 +22,19 @@ return(
    variants={pageAnimation} 
    initial="hidden" 
    animate="show"> 
+      <motion.div variants={sliderContainer}>
+         <Frame1 variants={slider}></Frame1>
+         <Frame2 variants={slider}></Frame2>
+         <Frame3 variants={slider}></Frame3>
+         <Frame4 variants={slider}></Frame4>
+      </motion.div>
+  
       <Movie>
-         <h2>The Athlete</h2>
-         <div className="line"></div>
+         <motion.h2 variants={fade}>The Athlete</motion.h2>
+         <motion.div variants={lineAnim} className="line"></motion.div>
          <Link to='/work/the-athlete'>
-            <img src={athlete} alt="athlete" />
+            <Hide>
+               <motion.img variants={photoAnim} src={athlete} alt="athlete" /> </Hide>
          </Link>
       </Movie>
 
@@ -64,7 +72,7 @@ const Movie = styled.div`
 padding:5rem;
 .line{
    height: 0.5rem;
-   background: #cccccc;
+   background: #23d997;
    margin-bottom: 3rem;
 }
 img{
@@ -72,6 +80,30 @@ img{
    height: 70vh;
    object-fit: cover;
 }
+`
+
+const Hide = styled.div`
+   overflow:hidden;
+`
+const Frame1= styled(motion.div)`
+   left:0;
+   position:fixed;
+   top:10%;
+   width:100%;
+   height:100vh;
+   background: #fffeff;
+   z-index:2;
+`
+const Frame2 = styled(Frame1)`
+   background: #ff8efb;
+`
+
+const Frame3 = styled(Frame1)`
+   background: #8ed2ff;
+`
+
+const Frame4 = styled(Frame1)`
+   background: #8e8e8e;
 `
 
 export default OurWork;
