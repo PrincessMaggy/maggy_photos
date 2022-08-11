93d683e2b6dcaeff3a00467bd40ec940a657bf74
PrincessMaggy <edoziemagdalene@gmail.com>
2022-08-11 09:47:58 +0100

    wave animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from "react";
 import home1 from "../img/home1.png";
 import {About, Description, Image, Hide} from "../styles";
 import { titleAnim, container, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 
 //framer-motion
@@ -34,6 +35,7 @@ const AboutSection =() =>{
                 <Image>
                     <motion.img  variants={photoAnim} src={home1} alt="guy with a camera"/>
                 </Image>
+                <Wave/>
             </About>
 )
 }
